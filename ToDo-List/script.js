ebe62c06d3c5b5b47d90135109e5154580ff1c14
Barani7804 <barani3566v@gmail.com>
2025-03-08 01:25:03 +0530

    Add files via upload

---
+++ ToDo-List/script.js
@@ -0,0 +1,48 @@
+const inputbox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+console.log(listContainer)
+
+document.getElementById("add-btn").addEventListener("click", addTask);
+
+addEventListener("keydown", (e) => {
+    if(e.key == "Enter"){
+        addTask()
+    }
+})
+function addTask() {
+    if (inputbox.value == "") {
+        alert("You Must Write Something!!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        let span = document.createElement("span");
+        span.innerHTML = "<i class='fa-solid fa-trash'></i>";
+        li.appendChild(span);
+        listContainer.appendChild(li);
+        inputbox.value = "";
+        saveData()
+    }
+}
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+listContainer.addEventListener("click", (e) => {
+    if(e.target.tagName.toUpperCase() === "LI") {
+        e.target.classList.toggle("checked")
+        saveData()
+    }else if(e.target.tagName.toUpperCase()=== "SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+    }
+})
+
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data")
+}
+
+showTask()
